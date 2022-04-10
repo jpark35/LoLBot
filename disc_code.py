4487fc2691ddd2328e6b1064d6be1e53f868a076
jpark35 <103374624+jpark35@users.noreply.github.com>
2022-04-10 10:29:01 -0500

    Add files via upload

---
+++ disc_code.py
@@ -0,0 +1,129 @@
+
+from discord.ext import commands
+import lolbot as lolbot
+
+TOKEN = "OTU1OTUyMDEwNzQ4OTE5ODY4.YjpJsA.hxd1VNbEhyfoeJUHmcu-4yvDzuM"
+
+bot = commands.Bot(command_prefix="!")
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} successfully logged in!')
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    
+    if message.content == 'hello':
+        await message.channel.send(f'Hi {message.author}')
+    if message.content == 'bye':
+        await message.channel.send(f'Goodbye {message.author}')
+
+    await bot.process_commands(message)
+    
+@bot.command()
+async def info(ctx, *, arg='None'):
+    
+    lolbot.user_input = arg
+    lolbot.set_me()
+    # print('>>>>',lolbot.me)
+    message = lolbot.get_ID()
+   
+    await ctx.send(message)
+
+@bot.command()
+async def ranksolo(ctx, *, arg='None'):
+    lolbot.user_input = arg
+    lolbot.set_me()
+    lolbot.get_stats()
+    # print('>>>>',lolbot.me)
+    message = lolbot.get_solo()
+    
+    await ctx.send(message)
+
+@bot.command()
+async def teamrank(ctx, arg1='None',arg2='None',arg3='None',arg4='None',arg5='None'):
+    lolbot.user_input = arg1
+    lolbot.set_me()
+    lolbot.get_stats()
+    # print('>>>>',lolbot.me)
+    message1 = lolbot.get_solo()
+    lolbot.user_input = arg2
+    lolbot.set_me()
+    lolbot.get_stats()
+    message2 = lolbot.get_solo()
+    lolbot.user_input = arg3
+    lolbot.set_me()
+    lolbot.get_stats()
+    message3 = lolbot.get_solo()
+    lolbot.user_input = arg4
+    lolbot.set_me()
+    lolbot.get_stats()
+    message4 = lolbot.get_solo()
+    lolbot.user_input = arg5
+    lolbot.set_me()
+    lolbot.get_stats()
+    message5 = lolbot.get_solo()
+    messages = [message1, message2, message3, message4, message5]
+
+    pre_message = ''
+    final_message = ''
+    count = 0
+    message = ''
+
+    for message in messages:
+        if count < 4:
+            if messages[count] != messages[count+1]:
+                pre_message = pre_message + "\n" + messages[count] + "\n"
+                count = count + 1
+    final_message = pre_message + "\n" + messages[4]
+    
+    await ctx.send(final_message)
+
+@bot.command()
+async def teamrank2(ctx, *args):
+    lolbot.user_input = args
+    lolbot.set_me()
+    lolbot.get_stats()
+    # print('>>>>',lolbot.me)
+    message = lolbot.get_solo()
+    await ctx.send(message)
+
+@bot.command()
+async def rankflex(ctx, *, arg='None'):
+    
+    lolbot.user_input = arg
+    lolbot.set_me()
+    lolbot.get_stats()
+    # print('>>>>',lolbot.me)
+    message = lolbot.get_flex()
+    
+    await ctx.send(message)
+
+# @bot.command()
+# async def mastery(ctx, *, arg1='None'):
+#     lolbot.user_input = arg1
+#     lolbot.set_me()
+#     lolbot.get_stats()
+#     # print('>>>>',lolbot.me)
+#     message = lolbot.get_mastery()
+#     print(message)
+#     await ctx.send(message)
+
+@bot.command()
+async def mastery(ctx, arg1, arg2 = 5):
+    lolbot.user_input = arg1
+    lolbot.max_val = arg2
+    lolbot.set_me()
+    lolbot.get_stats()
+    # print('>>>>',lolbot.me)
+    message = lolbot.get_mastery()
+    
+    await ctx.send(message)
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Ping")
+
+bot.run(TOKEN)
---
+++ lolbot.py
@@ -0,0 +1,132 @@
+
+from riotwatcher import LolWatcher, ApiError
+from champDict import champ_dict
+
+api_key = 'RGAPI-30838856-e2c7-4527-a20f-df3ad919a78f'
+watcher = LolWatcher(api_key)
+my_region = 'na1'
+user_input = ''
+mastery_str = ''
+count = 0
+final_str = ''
+champion_mastery = ''
+all_champs = ''
+max_val = 0
+winrate = 0
+team_str = ''
+indiv_str = ''
+
+me = ''
+ranked_stats = ''
+
+def set_me():
+    global me
+    if user_input != 'None':
+        me = watcher.summoner.by_name(my_region, user_input)
+
+def get_ID():
+    ID_str = "IDENTIFICATION: \n" + "\n Summoner Name: " + me['name'] + "\n Summoner Level: " + str(me['summonerLevel'])
+    return ID_str
+
+def get_stats():
+    global ranked_stats
+    ranked_stats = watcher.league.by_summoner(my_region, me['id'])
+    print(ranked_stats)
+
+def get_solo():
+    
+    for stats in ranked_stats:
+        global winrate
+        winrate = stats['wins']/(stats['wins']+stats['losses']) * 100
+        # print(i['queueType'])
+        if stats['queueType'] == 'RANKED_SOLO_5x5':
+            solo_str = "RANKED SOLO: \n" + \
+                        "Summoner Name: " + stats['summonerName'] + \
+                        "\nRank: " + stats['tier'] + " " + stats['rank'] + \
+                        "\nWins - Losses: " + str(stats['wins']) + "-" + str(stats['losses']) + " (" + str(winrate) + "%)"\
+                        "\nHot Streak?: " + str(stats['hotStreak'])
+            return solo_str 
+            break
+
+def get_team():
+
+    global team_str
+    team_str = ''
+    global indiv_str
+    indiv_str = ''
+
+    for stats in ranked_stats:
+        global winrate
+        winrate = stats['wins']/(stats['wins']+stats['losses']) * 100
+        # print(i['queueType'])
+        if stats['queueType'] == 'RANKED_SOLO_5x5':
+            indiv_str = "RANKED SOLO: \n" + \
+                        "Summoner Name: " + stats['summonerName'] + \
+                        "\nRank: " + stats['tier'] + " " + stats['rank'] + \
+                        "\nWins - Losses: " + str(stats['wins']) + "-" + str(stats['losses']) + " (" + str(winrate) + "%)"\
+                        "\nHot Streak?: " + str(stats['hotStreak'])
+        
+            team_str = team_str + indiv_str            
+            return team_str 
+
+def get_flex():
+    for stats in ranked_stats:
+        # print(i['queueType'])
+        if stats['queueType'] == 'RANKED_FLEX_SR':
+            flex_str = "RANKED FLEX: \n" + \
+                        "Summoner Name: " + stats['summonerName'] + \
+                        "\nQueue Type: " + stats['queueType'] + \
+                        "\nRank: " + stats['tier'] + " " + stats['rank'] + \
+                        "\nWins - Losses: " + str(stats['wins']) + "-" + str(stats['losses']) + \
+                        "\nHot Streak?: " + str(stats['hotStreak'])
+            return flex_str
+                    
+
+def get_mastery():
+    global champion_mastery
+    champion_mastery = watcher.champion_mastery.by_summoner(my_region, me['id'])
+
+# count = 0
+# num = 0
+# for champ in champion_mastery:
+#     for stat in champion_mastery[count]:
+#         print("#", count+1, "Champion Info:", stat, ":", champion_mastery[count][stat])
+#     count = count + 1
+    global all_champs
+    all_champs = champ_dict
+# for champID in all_champs:
+#     print(champID, all_champs[champID])
+    global count
+    count = 0
+
+    final_str = ''
+
+    for champ in champion_mastery:
+
+        print(champ)
+
+        # mastery_str = ("\nCHAMPION MASTERY: " + "\n#" + str(count+1) + " Champion Mastery Information:" + \
+        #                 "\nChampion ID: " + str(champion_mastery[count]['championId']) + \
+        #                 "\nChampion Name: " + all_champs[champion_mastery[count]['championId']] + \
+        #                 "\nChampion Points Until Next Level: " + str(champion_mastery[count]['championPointsUntilNextLevel']) + \
+        #                 "\nChampion Chest Availability: " + str(champion_mastery[count]['chestGranted']) + \
+        #                 "\nChampion Level: " + str(champion_mastery[count]['championLevel']))  
+
+        mastery_str = ("\n#" + str(count+1) + " Champion Mastery Information:" + \
+                        "\nChampion ID: " + str(champ['championId']) + \
+                        "\nChampion Name: " + all_champs[champ['championId']] + \
+                        "\nChampion Points Until Next Level: " + str(champ['championPointsUntilNextLevel']) + \
+                        "\nChampion Chest Received?: " + str(champ['chestGranted']) + \
+                        "\nChampion Level: " + str(champ['championLevel'])+ "\n")  
+
+
+        final_str = final_str + mastery_str
+
+        print(final_str)
+
+        count = count + 1
+        global max_val
+        if count == max_val:
+            break
+
+    return final_str 
